3b29952827a7aa1b3bc803715bb342432cdde17e
gracie lundell and Kyle Derrick <gracielundell@gmail.com>
2016-03-15 16:48:31 -0700

    add button toggles

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -4,6 +4,7 @@ var toFahrenheit = require('./../js/temperature.js').toFahrenheit;
 var apiKey = "19e5e8222f081c8bd019804334478683";
 
 $(document).ready(function() {
+  $("#showCelcius").hide();
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
@@ -11,9 +12,26 @@ $(document).ready(function() {
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
       // console.log(response);
       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%. ");
-      $('.showWeather').append("The temperature in " + city + " is " + toCelcius(parseInt(response.main.temp)) + "C. ");
-      $('.showWeather').append("The temperature in " + city + " is " + toFahrenheit(parseInt(response.main.temp)) + "F. ");
-    });
+      $('.showCelcius').text("The temperature in " + city + " is " + toCelcius(parseInt(response.main.temp)) + "C. ");
+      $('.showFahrenheit').text("The temperature in " + city + " is " + toFahrenheit(parseInt(response.main.temp)) + "F. ");
+      $('.showFahrenheit').hide();
 
+    });
+    $("#showFah").click(function(){
+      $('.showFahrenheit').show();
+      $('#showFah').hide();
+      $("#showCelcius").show();
+      $(".showCelcius").hide();
+      $("#showFah").click(function(){
+        $('.showFahrenheit').show();
+        $('.showCelcius').hide();
+        $("#showFah").hide();
+        $("#showCelcius").show();
+
+      });
+
+    });
   });
+
+
 });
